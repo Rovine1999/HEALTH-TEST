bc6ddc5dd3e2fc8e56ce1ebaf32c3e5e9f89a915
Rovine1999 <rovinewanjala99@gmail.com>
2020-10-24 22:07:34 +0300

    add

---
+++ js/script.js
@@ -0,0 +1,152 @@
+function myFunction() {
+    var disease = $("#Disease").val();
+    if (disease === "Malaria"){
+        confirm("1). Fever");
+        confirm("2). Fatigue");
+        confirm("3). Body ache");
+        confirm("4). Head ache");
+        confirm("5). Cough");
+        confirm("6). Shivering");
+        var number = parseInt(prompt("How many did you check as ok?"));
+        if (number > 4){
+            alert("Visit nearest health facilty")
+        } else if(number < 4){
+            alert("Get some rest")
+        }
+        
+    } 
+    
+    if (disease === "Covid-19"){
+        confirm("1). Aches and Pain");
+        confirm("2). Sore throat");
+        confirm("3). Diarrhoea");
+        confirm("4). Headache");
+        confirm("5). Loss of taste or smell");
+        confirm("6). Rush on the skin or discolouration of fingers and toes");
+        var number = parseInt(prompt("How many did you check as ok?"));
+        if (number > 4){
+            alert("Visit nearest health facilty")
+        } else if(number < 4){
+            alert("Get some rest")
+        }
+        
+    }
+
+    if (disease === "Typhoid"){
+        confirm("1). Diarrhea or Costipation");
+        confirm("2). Stomach pain");
+        confirm("3). Loss of Appetite");
+        confirm("4). Cough");
+        confirm("5). Headache");
+        confirm("6). Weakness");
+        var number = parseInt(prompt("How many did you check as ok?"));
+        if (number > 4){
+            alert("Visit nearest health facilty")
+        } else if(number < 4){
+            alert("Get some rest")
+        }
+        
+    }
+
+    if (disease === "Tuberculosis"){
+        confirm("1). Coughing");
+        confirm("2). Coughing up blood");
+        confirm("3). Chills");
+        confirm("4). Night Sweat");
+        confirm("5). Chest Pain");
+        confirm("6). Fatigue");
+        var number = parseInt(prompt("How many did you check as ok?"));
+        if (number > 4){
+            alert("Visit nearest health facilty")
+        } else if(number < 4){
+            alert("Get some rest")
+        }
+        
+    }
+   
+    if (disease === "Diabetes"){
+        confirm("1). Urinating Often");
+        confirm("2). Weight Loss");
+        confirm("3). Extrem Fatigue");
+        confirm("4). Blurry Vission");
+        confirm("5). Feeling Very Hungry");
+        confirm("6). Feeling Very Thirsty");
+        var number = parseInt(prompt("How many did you check as ok?"));
+        if (number > 4){
+            alert("Visit nearest health facilty")
+        } else if(number < 4){
+            alert("Get some rest")
+        }
+        
+    } if (disease === "Ulcers"){
+        confirm("1). Dull pain in the stomach");
+        confirm("2). Weight Loss");
+        confirm("3). Burping or acid reflux");
+        confirm("4). Bloating");
+        confirm("5). Feeling easly full");
+        confirm("6). Burping sensation in chest");
+        var number = parseInt(prompt("How many did you check as ok?"));
+        if (number > 4){
+            alert("Visit nearest health facilty")
+        } else if(number < 4){
+            alert("Get some rest")
+        }
+        
+    } if (disease === "Heart Attack"){
+        confirm("1). Heartburn");
+        confirm("2). Indigestion");
+        confirm("3). Naussea");
+        confirm("4). Cold Sweat");
+        confirm("5). Fatigue");
+        confirm("6). Shortness Of Breath");
+        var number = parseInt(prompt("How many did you check as ok?"));
+        if (number > 4){
+            alert("Visit nearest health facilty")
+        } else if(number < 4){
+            alert("Get some rest")
+        }
+        
+    }   if (disease === "Cancer"){
+        confirm("1). Lump or area of thickening that can be felt under skin");
+        confirm("2). Fatigue");
+        confirm("3). Skin changes such as yellowing and darkening");
+        confirm("4). Persistence cough or trouble breathing");
+        confirm("5). Weight changes i.e uninteded gain or loss");
+        confirm("6). Changes in bowel or bladder habbits");
+        var number = parseInt(prompt("How many did you check as ok?"));
+        if (number > 4){
+            alert("Visit nearest health facilty")
+        } else if(number < 4){
+            alert("Get some rest")
+        }
+        
+    } if (disease === "HIV/AIDS"){
+        confirm("1). Rapid weight loss");
+        confirm("2). Pneumonia");
+        confirm("3). Reccuring Fever");
+        confirm("4). Extreme and unexplained tiredness.");
+        confirm("5). Diarrhea that last more than a week");
+        confirm("6). Sore of the mouth anus or genital");
+        var number = parseInt(prompt("How many did you check as ok?"));
+        if (number > 4){
+            alert("Visit nearest health facilty")
+        } else if(number < 4){
+            alert("Get some rest")
+        }
+        
+    } if (disease === "Pneumonia"){
+        confirm("1). Cough");
+        confirm("2). Fever");
+        confirm("3). Shortness of breath");
+        confirm("4). Rapid, shallow breathing.");
+        confirm("5). Loss of appetite, loss energy and fatigue");
+        confirm("6). Sharp or stabbing chest pain.");
+        var number = parseInt(prompt("How many did you check as ok?"));
+        if (number > 4){
+            alert("Visit nearest health facilty")
+        } else if(number < 4){
+            alert("Get some rest")
+        }
+        
+    }                             
+ }
